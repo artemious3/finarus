7e84aa374e04c8e9e7d475144a4a13736d101108
Artsiom Padhaiski <artempodgaisky@gmail.com>
2025-03-16 12:10:16 +0300

    deposits : more valid way to calculate deposits

--- src/server/services/deposit.rs
+++ src/server/services/deposit.rs
@@ -5,6 +5,7 @@ use l1::common::deposit::Deposit;
 use crate::traits::dynamic::Dynamic;
 use std::collections::hash_map::HashMap;
 use log::*;
+use chrono::{DateTime, Utc};
 
 
 use chrono::Datelike;
@@ -47,6 +48,23 @@ impl DepositService {
         }
     }
 
+
+}
+
+
+type UnitSize = i32;
+
+pub fn signed_month_difference(start: &DateTime<Utc>, end: &DateTime<Utc>) -> UnitSize {
+    let end_naive = end.date_naive();
+    let start_naive = start.date_naive();
+
+    let month_diff = end_naive.month() as UnitSize - start_naive.month() as UnitSize;
+    let years_diff = (end_naive.year() - start_naive.year()) as UnitSize;
+    if month_diff >= 0 {
+        (years_diff * 12) + month_diff
+    } else {
+        (years_diff - 1) * 12 + (month_diff + 12)
+    }
 }
 
 
@@ -55,13 +73,19 @@ impl Dynamic for DepositService {
         for (_, vec) in &mut self.deposits {
             for deposit in vec {
                 
-                let months = now.month0() as i32 - deposit.last_update.month0() as i32 - 
-                            (if now.day0() < deposit.last_update.day0() {1} else {0});
-                if months < 0 {
-                    error!("Now is the past, last update is the future!");
-                    error!("Skipping deposit.");
+                //relative to last update
+                let months_to_now = signed_month_difference(&deposit.last_update, now);
+                let months_to_end = signed_month_difference(&deposit.last_update, &deposit.end_date);
+
+                if months_to_end < 0 {
+                    info!("Deposit skipped since it is overdue");
                     continue;
-                } else if months > 0 {
+                } else if months_to_now < 0 {
+                    error!("Now is the past, last update is the future!");
+                    error!("Something is wrong. Skipping deposit.");
+                    continue;
+                } else  {
+                    let months = std::cmp::min(months_to_now, months_to_end);
                     deposit.last_update = now.clone();
                     let koef = (1.0 + (deposit.interest_rate as f64)/100.0/12.0).powi(months);
                     deposit.current_amount = ((deposit.current_amount as f64) * koef).floor() as Money;
